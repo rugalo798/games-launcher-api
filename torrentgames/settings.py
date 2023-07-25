654a4884e7bc4b21f53a6ad8c5f4dcb0d6354c8d
Rugalo798 <tiagovinicius79810@gmail.com>
2023-07-25 01:02:14 -0300

    Create super user admin account

--- torrentgames/settings.py
+++ torrentgames/settings.py
@@ -130,3 +130,5 @@ MEDIA_URL = '/media/photos/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS=['*']
